7e6857d9bc1d09d2ea953a3ac90c75b9ccac1099
GiantSlayer1913 <michaeljaredfisch@gmail.com>
2018-05-26 14:29:01 -0400

    Changed production url in config to chris-sale-fan-page-api heroku

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: '<replace-with-heroku-url>',
+  production: 'https://chris-sale-fan-page-api.herokuapp.com/',
   development: 'http://localhost:4741'
 }
 
