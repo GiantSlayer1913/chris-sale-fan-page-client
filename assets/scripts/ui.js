bed16690e784265bf4d126aba2f3d778daeb75bc
GiantSlayer1913 <michaeljaredfisch@gmail.com>
2018-05-28 18:39:01 -0400

    Added auth forms and scripts
    Added auth forms to index html
    Added classic auth scripts to assets/scripts
    scripts include classic auth api, events, and ui

---
+++ assets/scripts/ui.js
@@ -0,0 +1,65 @@
+'use strict'
+const store = require('./store')
+
+// Auth Ui below
+const signUpSuccess = function () {
+  $('#message').text('Successfully signed up')
+  $('#message').css('background-color', 'green')
+  $('#sign-up').trigger('reset')
+}
+const signUpFailure = function () {
+  $('#message').text('Failure signing up')
+  $('#message').css('background-color', 'red')
+  $('#sign-up').trigger('reset')
+}
+const signInSuccess = function (data) {
+  $('#message').text('Successfully signed in')
+  $('#message').css('background-color', 'green')
+  $('#change-password').css('display', 'block')
+  $('#sign-out').css('display', 'block')
+  $('#sign-up').css('display', 'none')
+  $('#sign-up').trigger('reset')
+  $('#sign-in').css('display', 'none')
+  $('#sign-in').trigger('reset')
+  store.user = data.user
+}
+const signInFailure = function () {
+  $('#message').text('Failure signing in')
+  $('#message').css('background-color', 'red')
+  $('#sign-in').trigger('reset')
+}
+
+const changePasswordSuccess = function () {
+  $('#message').text('Successfully changed password')
+  $('#message').css('background-color', 'green')
+  $('#change-password').trigger('reset')
+}
+const changePasswordFailure = function () {
+  $('#message').text('Failure to change password')
+  $('#message').css('background-color', 'red')
+  $('#change-password').trigger('reset')
+}
+const signOutSuccess = function (data) {
+  $('#message').text('Successfully signed out')
+  $('#message').css('background-color', 'green')
+  $('#sign-up').css('display', 'block')
+  $('#sign-in').css('display', 'block')
+  $('#change-password').css('display', 'none')
+  $('#sign-out').css('display', 'none')
+  store.user = null
+}
+const signOutFailure = function () {
+  $('#message').text('Failure to sign out')
+  $('#message').css('background-color', 'red')
+}
+
+module.exports = {
+  signUpSuccess,
+  signUpFailure,
+  signInSuccess,
+  signInFailure,
+  changePasswordSuccess,
+  changePasswordFailure,
+  signOutSuccess,
+  signOutFailure
+}
